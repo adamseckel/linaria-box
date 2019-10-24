a45266b6168cbb897dd6b7e7c480d1500b48d916
Adam Seckel <adam.seckel@gmail.com>
2019-10-24 20:26:10 +0200

    Bootstrap library

---
+++ index.ts
@@ -0,0 +1,61 @@
+import { styled } from 'linaria/react';
+
+const justifyMap = {
+  start: 'flex-start',
+  end: 'flex-end',
+  'space-between': 'space-between',
+  'space-around': 'space-around',
+  center: 'center',
+  'space-evenly': 'space-evenly',
+};
+
+const alignMap = {
+  start: 'flex-start',
+  end: 'flex-end',
+  center: 'center',
+  baseline: 'baseline',
+  stretch: 'stretch',
+};
+
+const justifyContent = ({ justify = 'center' }: { justify: Justify }) =>
+  justifyMap[justify];
+
+const alignItems = ({ align = 'center' }: { align: Align }) => alignMap[align];
+
+type Justify =
+  | 'start'
+  | 'end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly';
+
+type Align = 'start' | 'end' | 'center' | 'baseline' | 'stretch';
+
+export interface Props {
+  wrap: boolean;
+  grow: boolean;
+  inline: boolean;
+  justify: Justify;
+  align: Align;
+}
+
+const Box = styled.div<Props>`
+  display: flex;
+  justify-content: ${justifyContent};
+  align-items: ${alignItems};
+  flex-wrap: ${props => (props.wrap ? 'wrap' : 'no-wrap')};
+  flex-grow: ${props => (props.grow ? 1 : 0)};
+  width: ${props => (props.inline ? 'auto' : '100%')};
+  position: relative;
+`;
+
+export const Row = styled(Box)`
+  flex-direction: row;
+`;
+
+export const Column = styled(Box)`
+  flex-direction: column;
+`;
+
+export default Box;
